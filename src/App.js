0de249707ded048e0e767f3a01e0e7ec96842d8d
sherrytetich <sheryl@kabarak.ac.ke>
2023-03-29 01:55:03 +0300

    adding functions to navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Navbar from "./components/Navbar/Navbar";
+import './App.css'
+
 function App() {
   return (
     <div className="App">
-      Subscribe ZainKeepsCode
+      <Navbar/>
     </div>
   );
 }
