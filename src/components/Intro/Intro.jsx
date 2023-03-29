a845b83e7ca7a1022ab8b3c0d3cc29ae5bf7b879
sherrytetich <sheryl@kabarak.ac.ke>
2023-03-29 04:25:29 +0300

    adding images to intro

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -3,6 +3,12 @@ import './Intro.css'
 import Github from '../../img/github.png';
 import LinkedIn from '../../img/linkedin.png';
 import Instagram from '../../img/instagram.png';
+import Vector1 from '../../img/Vector1.png';
+import Vector2 from '../../img/Vector2.png';
+import boy from '../../img/boy.png';
+import thumbup from '../../img/thumbup.png';
+import Crown from '../../img/crown.png';
+import glassesimoji from '../../img/glassesimoji.png';
 function Intro() {
   return (
     <div className="intro">
@@ -14,13 +20,20 @@ function Intro() {
         </div>
         <button className="button i-button">Hire Me</button>
         <div className="i-icons">
+            <a href="https://github.com/sherrytetich">
             <img src={Github} alt="" />
+            </a>
             <img src={LinkedIn} alt="" />
             <img src={Instagram} alt="" />
         </div>
         </div>
         <div className="i-right">
-            This is where to display my image
+            <img src={Vector1} alt="" />
+            <img src={Vector2} alt="" />
+            <img src={boy} alt="" />
+            <img src={thumbup} alt="" />
+            <img src={Crown} alt="" />
+            <img src={glassesimoji} alt="" />
         </div>
     </div>  
   );
